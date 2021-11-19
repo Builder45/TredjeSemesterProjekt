73b776a7a652663a3971d65feab0b67c6372c7c0
Builder45 <70648189+Builder45@users.noreply.github.com>
2021-11-19 09:59:17 +0100

    Persistence init

---
+++ BeboerWeb/BeboerWeb.Persistence/BeboerWebContext.cs
@@ -0,0 +1,11 @@
+﻿using BeboerWeb.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BeboerWeb.Persistence
+{
+    public class BeboerWebContext:DbContext
+    {
+
+    }
+}
