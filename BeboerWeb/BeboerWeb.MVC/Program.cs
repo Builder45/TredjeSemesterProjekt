e29659a99e9a1fea87ead39ba3818d4285b27259
Builder45 <70648189+Builder45@users.noreply.github.com>
2021-11-22 11:27:51 +0100

    Register stuff

--- BeboerWeb/BeboerWeb.MVC/Program.cs
+++ BeboerWeb/BeboerWeb.MVC/Program.cs
@@ -20,6 +20,17 @@ builder.Services.AddAuthorization(options =>
         policy.RequireClaim("IsAdmin", "Yes"));
 });
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
