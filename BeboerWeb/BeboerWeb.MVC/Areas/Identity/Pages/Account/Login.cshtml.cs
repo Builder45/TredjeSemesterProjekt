df55bc342d596bebf6a1b3162bf2d4b81ac3e58b
rasmus5500 <74899173+rasmus5500@users.noreply.github.com>
2021-11-24 10:53:13 +0100

    Revert "BA view"
    This reverts commit 505aae8dd52d61bc55ef7e956d1d6c607b278386.

--- BeboerWeb/BeboerWeb.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BeboerWeb/BeboerWeb.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -103,7 +103,6 @@ namespace BeboerWeb.MVC.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-
             returnUrl ??= Url.Content("~/");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
