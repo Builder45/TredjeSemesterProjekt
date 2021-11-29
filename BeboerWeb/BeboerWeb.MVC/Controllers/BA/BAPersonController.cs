5d30faa2f41c0ba4a878cb6745af0f01ff211365
Builder45 <70648189+Builder45@users.noreply.github.com>
2021-11-29 19:56:29 +0100

    Controller cleanup + Access Denied scaffolding

--- BeboerWeb/BeboerWeb.MVC/Controllers/BA/BAPersonController.cs
+++ BeboerWeb/BeboerWeb.MVC/Controllers/BA/BAPersonController.cs
@@ -7,16 +7,19 @@ using BeboerWeb.MVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BeboerWeb.MVC.Controllers.BA
 {
-    [Route("Dashboard/Admin/Brugere/[action]")]
+    [Authorize(Policy = "BA")]
+    [Route("dashboard/admin/brugere/")]
     public class BAPersonController : Controller
     {
-
         private readonly IPersonService _personService;
         private readonly ApplicationDbContext _userDb;
         private readonly UserManager<IdentityUser> _userManager;
+
+        private string viewPath = "Views/Dashboard/BA/Person";
 
         public BAPersonController(IPersonService personService, ApplicationDbContext userDb, UserManager<IdentityUser> userManager)
         {
@@ -25,6 +28,7 @@ namespace BeboerWeb.MVC.Controllers.BA
             _userManager = userManager;
         }
 
+        [Route("")]
         public async Task<ActionResult> Index()
         {
             var model = new List<BrugerViewModel>();
@@ -43,38 +47,16 @@ namespace BeboerWeb.MVC.Controllers.BA
 
                 model.Add(brugerModel);
             }
-            return View("Views/Dashboard/BA/Person/Index.cshtml", model);
+            return View($"{viewPath}/Index.cshtml", model);
         }
 
-        // GET: BrugerController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View("Views/Dashboard/BA/Person/Details.cshtml");
-        }
-
-        // GET: BrugerController/Create
+        [Route("opret")]
         public ActionResult Create()
         {
-            //return View("Areas/Identity/Pages/Account/Register.cshtml");
             return Redirect("~/Identity/Account/Register");
         }
 
-        // POST: BrugerController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: BrugerController/Edit/5
+        [Route("rediger")]
         public async Task<ActionResult> Edit(Guid id)
         {
             var personModel = await _personService.GetPersonByPersonIdAsync(id);
@@ -87,10 +69,9 @@ namespace BeboerWeb.MVC.Controllers.BA
                 model.Email = bruger.Email;
             }
 
-            return View("Views/Dashboard/BA/Person/Edit.cshtml", model);
+            return View($"{viewPath}/Edit.cshtml", model);
         }
 
-        // POST: BrugerController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task <ActionResult> Edit(BrugerViewModel bruger)
@@ -100,9 +81,10 @@ namespace BeboerWeb.MVC.Controllers.BA
                 await _personService.UpdatePersonAsync(new PersonDTO { BrugerId = bruger.BrugerId, Fornavn = bruger.Fornavn, Efternavn = bruger.Efternavn, Id = bruger.Id, Telefonnr = bruger.Telefonnr});
                 return RedirectToAction(nameof(Index));
             }
-            return View("Views/Dashboard/BA/Person/Edit.cshtml", bruger);
+            return View($"{viewPath}/Edit.cshtml", bruger);
         }
 
+        [Route("roller")]
         public async Task<ActionResult> EditPolicies(Guid id)
         {
             var model = new UserPolicyViewModel();
@@ -119,7 +101,7 @@ namespace BeboerWeb.MVC.Controllers.BA
                 model.IsVV = true;
             }
 
-            return View("Views/Dashboard/BA/Person/EditPolicies.cshtml", model);
+            return View($"{viewPath}/EditPolicies.cshtml", model);
         }
 
         [HttpPost]
@@ -140,27 +122,6 @@ namespace BeboerWeb.MVC.Controllers.BA
             }
 
             return RedirectToAction(nameof(Index));
-        }
-
-        // GET: BrugerController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: BrugerController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
