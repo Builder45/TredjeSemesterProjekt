5427f1f1a294b9ab245e3d0707e5a66fc5f65ba9
Builder45 <70648189+Builder45@users.noreply.github.com>
2021-11-30 10:36:28 +0100

    LinkVicevaert Use case

---
+++ BeboerWeb/BeboerWeb.Application/UseCases/VicevaertUC/Interfaces/ILinkVicevaertUseCase.cs
@@ -0,0 +1,10 @@
+﻿using BeboerWeb.Application.Requests.Vicevaert;
+
+namespace BeboerWeb.Application.UseCases.VicevaertUC.Interfaces
+{
+    public interface ILinkVicevaertUseCase
+    {
+        public void LinkVicevaert(LinkVicevaertRequest command); 
+        public void UnlinkVicevaert(LinkVicevaertRequest command);
+    }
+}
---
+++ BeboerWeb/BeboerWeb.Persistence/Repositories/VicevaertRepository.cs
@@ -0,0 +1,27 @@
+﻿using BeboerWeb.Application.Persistence;
+using BeboerWeb.Domain.Models;
+
+namespace BeboerWeb.Persistence.Repositories
+{
+    public class VicevaertRepository : IVicevaertRepository
+    {
+        private readonly BeboerWebContext _db;
+
+        public VicevaertRepository(BeboerWebContext db)
+        {
+            _db = db;
+        }
+
+        public void LinkVicevaert(Vicevaert vicevaert)
+        {
+            _db.Vicevaert.Add(vicevaert);
+            _db.SaveChanges();
+        }
+
+        public void UnlinkVicevaert(Vicevaert vicevaert)
+        {
+            _db.Vicevaert.Remove(vicevaert);
+            _db.SaveChanges();
+        }
+    }
+}
