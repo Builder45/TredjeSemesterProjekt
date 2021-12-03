e13840a735bfd6bd9db1b0b3f5253b441d0daa54
Builder45 <70648189+Builder45@users.noreply.github.com>
2021-12-03 12:29:56 +0100

    Lejer stuff

--- BeboerWeb/BeboerWeb.MVC/Models/LejerViewModel.cs
+++ BeboerWeb/BeboerWeb.MVC/Models/LejerViewModel.cs
@@ -8,13 +8,19 @@ namespace BeboerWeb.MVC.Models
         public Guid Id { get; set; }
 
         [Display(Name = "Start på lejeperiode")]
+        [DataType(DataType.Date)]
         public DateTime LejeperiodeStart { get; set; }
 
         [Display(Name = "Ende på lejeperiode")]
+        [DataType(DataType.Date)]
         public DateTime LejeperiodeSlut { get; set; }
+
         public Guid LejemaalId { get; set; }
 
         public List<Guid> PersonIds { get; set; } = new List<Guid>();
+
+        [Display(Name = "Lejer(e)")]
+        public List<string> LejerNavne { get; set; } = new List<string> {"Tomgang"};
 
         public void AddDataFromDto(LejerDTO dto)
         {
@@ -23,6 +29,13 @@ namespace BeboerWeb.MVC.Models
             LejeperiodeSlut = dto.LejeperiodeSlut;
             LejemaalId = dto.LejemaalId;
             PersonIds = dto.PersonIds;
+
+            if (dto.LejerNavne.Count == 0) return;
+            LejerNavne.Remove("Tomgang");
+            foreach (var navn in dto.LejerNavne)
+            {
+                LejerNavne.Add(navn);
+            }
         }
     }
 }
