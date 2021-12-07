3422912423789119d79bc96d3021360251993c73
rasmus5500 <74899173+rasmus5500@users.noreply.github.com>
2021-12-07 11:29:15 +0100

    Mit lejemål færdig part 2

--- BeboerWeb/BeboerWeb.MVC/Models/LejemaalLejerViewModel.cs
+++ BeboerWeb/BeboerWeb.MVC/Models/LejemaalLejerViewModel.cs
@@ -4,30 +4,9 @@ namespace BeboerWeb.MVC.Models
 {
     public class LejemaalLejerViewModel
     {
-        //public Guid Id { get; set; }
-        //public string Adresse { get; set; }
-        //public string Etage { get; set; }
-        //public double Husleje { get; set; }
-        //public double Areal { get; set; }
-        //public bool Koekken { get; set; }
-        //public bool Badevaerelse { get; set; }
-
         public LejemaalViewModel Lejemaal { get; set; } = new LejemaalViewModel();
         public List<LejerViewModel> Lejere { get; set; } = new List<LejerViewModel>();
 
-        //public LejemaalDTO GetLejemaalDTO()
-        //{
-        //    return new LejemaalDTO()
-        //    {
-        //        Id = Lejemaal.Id,
-        //        Adresse = Lejemaal.Adresse,
-        //        Areal = Lejemaal.Areal,
-        //        Etage = Lejemaal.Etage,
-        //        Husleje = Lejemaal.Husleje,
-        //        Badevaerelse = Lejemaal.Badevaerelse,
-        //        Koekken = Lejemaal.Koekken
-        //    };
-        //}
         public void AddDataFromDto(LejemaalDTO dto)
         {
             Lejemaal.Id = dto.Id;
